52e084866a719f047ed98f31941cc9cf11d20dca
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-22 08:32:20 -0700

    [Problem 1] parameterize V and omega

--- src/Prob1/ProblemOne.m
+++ src/Prob1/ProblemOne.m
@@ -5,3 +5,16 @@ fprintf('Infinity value: %d\n', var.Constants.Infinity);
 
 fprintf('Testing square wave with input of t=1\n');
 results = SquareWave(1,2*pi,1);
+if ~isempty(results)
+    fprintf('results is not empty\n');
+end 
+
+
+y = zeros(1,((abs(var.Constants.tmin) + abs(var.Constants.tmax))/var.Constants.tinc));
+idx = 1;
+for t = var.Constants.tmin : var.Constants.tinc : var.Constants.tmax
+    y(1,idx) = SquareWave(t,eval(var.Constants.omega),var.Constants.tinc);
+    idx = idx + 1;
+end
+
+plot(t,y);
