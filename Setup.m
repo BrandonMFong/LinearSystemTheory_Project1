b6b319720debf09b0fc633af4719547d5012b717
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-22 08:03:58 -0700

    [Problem 1] Just using the var variable to get the constant values instead of declaring another variable

--- Setup.m
+++ Setup.m
@@ -1,20 +1,27 @@
 format long
 clear
 try 
+    var = jsondecode(fileread('Project.json'));
+
+    % make output dir
+    % probably do not need this 
     if ~exist('Outputs', 'dir')
         mkdir('Outputs')
     end 
-    var = jsondecode(fileread('Project.json'));
+    
+    % Adding paths for project
     [rows, columns] = size(var.Paths);
     for r = 1:rows
         addpath(var.Paths(r).Path);
     end
+
+    % Define constants used in this project
+    % for c = 1:size(var.Constants.Constant,1)
+    %     fprintf('Name: %s\n', var.Constants(c).Constant.Name);
+    %     fprintf('Value: %s\n', var.Constants(c).Constant.Value);
+    %     eval([var.Constants(c).Constant.Name , ' = ', var.Constants(c).Constant.Value, ';']);
+    % end 
+
 catch exception
     throw(exception)
 end
-
-% Defining constants
-% const = Constants();
-
-var = jsondecode(fileread('Project.json'));
-% TODO foreach each constant, eval the dynamic variable
--- src/Prob1/ProblemOne.m
+++ src/Prob1/ProblemOne.m
@@ -1,3 +1,4 @@
 % Fourier series of a square wave.
 
 fprintf('Problem 1!\n');
+fprintf('Infinity value: %d!\n', var.Constants.Infinity);
