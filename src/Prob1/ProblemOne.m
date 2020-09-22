859cf90af7ec38002362f890fb20ad14cb534f59
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-22 08:49:06 -0700

    [Problem 1] waitbar

--- src/Prob1/ProblemOne.m
+++ src/Prob1/ProblemOne.m
@@ -9,12 +9,16 @@ if ~isempty(results)
     fprintf('results is not empty\n');
 end 
 
-
-y = zeros(1,((abs(var.Constants.tmin) + abs(var.Constants.tmax))/var.Constants.tinc));
+max = ((abs(var.Constants.tmin) + abs(var.Constants.tmax))/var.Constants.tinc);
+y = zeros(1,max);
 idx = 1;
+
+StatusRowOut = waitbar(0,sprintf('Calculating Square wave (main loop)')); % init progress
 for t = var.Constants.tmin : var.Constants.tinc : var.Constants.tmax
     y(1,idx) = SquareWave(t,eval(var.Constants.omega),var.Constants.tinc);
     idx = idx + 1;
+    waitbar((idx)/(max),StatusRowOut,sprintf('Calculating Square wave (main loop)'));  % update progress
 end
+close(StatusRowOut); % terminate progress
 
 plot(t,y);
--- src/Prob1/SquareWave.m
+++ src/Prob1/SquareWave.m
@@ -1,10 +1,13 @@
 function out = SquareWave(t, omega, V)
     global var;
     out = 0;
+    StatusRow = waitbar(0,sprintf('Calculating Square wave (inner loop)')); % init progress
     for k = 1:2:var.Constants.Infinity % Inc by 2 for odd 
         A = 4*V;
         B = A/(k*pi);
         C = B*sin(k*omega*t);
         out = out + C;
+        waitbar((k)/(var.Constants.Infinity),StatusRow,sprintf('Calculating Square wave (inner loop)'));  % update progress
     end
+    close(StatusRow); % terminate progress
 end
