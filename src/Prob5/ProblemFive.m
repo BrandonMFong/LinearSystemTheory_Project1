2c3f2066cf311fc6209240f38c713c51c681ae06
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-23 13:46:31 -0700

    Init problem 5

---
+++ src/Prob5/ProblemFive.m
@@ -0,0 +1,20 @@
+% Rectangular function and its Fourier Transform
+
+fprintf('Problem 4!\n');
+
+max = ((abs(const.Problem4.tmin) + abs(const.Problem4.tmax))/const.Problem4.tinc);
+y = zeros(1,max);
+idx = 1;
+StatusRowOut = waitbar(0,sprintf('Calculating Rect function (main loop)')); % init progress
+for t = const.Problem4.tmin : const.Problem4.tinc : const.Problem4.tmax
+    y(1,idx) = const.Problem4.V * rect((t-const.Problem4.t0)/const.Problem4.T);
+    idx = idx + 1;
+    waitbar((idx)/(max),StatusRowOut,sprintf('Calculating Rect function (main loop)'));  % update progress
+end
+close(StatusRowOut); % terminate progress
+
+t = const.Problem4.tmin : const.Problem4.tinc : const.Problem4.tmax
+% hold on 
+figure('Name','Rect');
+plot(t,y);
+ylim([const.Problem4.ymin const.Problem4.ymax]);
